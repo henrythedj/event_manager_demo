e0578649a1945d2c649fba0c5bc1540b86c36d03
Henry Harding McCall <hardingmccall@gmail.com>
2016-11-21 17:14:23 -0600

    cleans zipcodes and displays legislators

---
+++ event_manager.rb
@@ -0,0 +1,31 @@
+require "csv"
+require "sunlight/congress"
+
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+
+def clean_zipcode(zipcode)
+	zipcode.to_s.rjust(5,"0")[0..4]
+end
+
+def legislators_by_zipcode(zipcode)
+	legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+
+	legislator_names = legislators.collect do |legislator|
+		"#{legislator.first_name} #{legislator.last_name}"
+	end
+	legislator_names.join(", ")
+end
+	
+
+puts "EventManager Initialized!"
+
+# contents = File.read "event_attendees.csv"
+# puts contents
+
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents.each do |line|
+	name = line[:first_name]
+	zipcode = clean_zipcode(line[:zipcode])
+	legislators = legislators_by_zipcode(zipcode)
+	puts "#{name} #{zipcode} #{legislators}"
+end
